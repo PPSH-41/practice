044dcbd034484efc834588753e124068286af5fc
PPSH-41 <3168317381@qq.com>
2020-02-05 11:37:41 +0800

    february 5th

---
+++ src/staticTest/StaticTest.java
@@ -0,0 +1,10 @@
+package com.practice.helloworld;
+
+public class StaticTest {
+    public static void main(String[] args) {
+//        three Employee objects to fill the staff array
+        Employee[] staff
+    }
+}
+
+
