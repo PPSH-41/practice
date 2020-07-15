0ce809ace39c2f53e5a1ab26358473be0f3e62ef
危爽 <3168317381@qq.com>
2020-07-15 11:08:21 +0800

    2020/07/15

--- src/JAVA/recursion/Filefilter/LambdaQuickly.java
+++ src/JAVA/recursion/Filefilter/LambdaQuickly.java
@@ -6,7 +6,7 @@ import java.io.FileFilter;
 public class LambdaQuickly {
  
         public static void main(String[] args) {
-            File file = new File("E:\\");
+            File file = new File("E:\\Program Files (x86)");
             getAllFile(file);
         }
 
